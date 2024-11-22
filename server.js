23fa2702ed653ccc5e4d2ee56cebe2d3c546fa90
careyarmst <careyarmst@earthlink.net>
2024-11-22 17:21:14 -0500

    Inital BackendVue Videos

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.get('/messages', (req, res) => {
+    const messages = ['yes', 'each', 'good times']
+    
+    res.send(messages)
+})
+
+app.listen(port, () => console.log('app running'))
